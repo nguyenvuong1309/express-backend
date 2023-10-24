6d580346b4a5949d01f44127bbdb0f7dc2990dce
Mona Lisa01 <21522809@gm.uit.edu.vn>
2023-10-24 10:45:14 +0700

    :)

---
+++ server.js
@@ -0,0 +1,24 @@
+// https://www.youtube.com/watch?v=H9M02of22z4&t=469s
+
+
+
+const express = require("express")
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
+const dotenv = require("dotenv").config();
+
+connectDb()
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+
+app.use(express.json());
+app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
